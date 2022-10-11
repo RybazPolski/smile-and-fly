1b9e208d3c69317af47a3d4882011332063d08be
Ryba <julorybarczyk@gmail.com>
2022-10-12 00:32:04 +0200

    pobieranie i zwracanie danych z itaki

--- node-js/app.js
+++ node-js/app.js
@@ -18,13 +18,17 @@ async function getTest(query){
     await page.waitForSelector(".cl-search-result");
 
     const results = await page.$$eval(".cl-search-result", elements => {
+            console.log("fetching data from DOM tree...")
+            let i = 0;
         return elements.map(el => {
             const properties = {};
             const titleEl = el.querySelector(".post-title .label");
             const title = el.querySelector(".post-title .label").innerHTML;
             properties.titleEl = titleEl;
             properties.title = title;
-            return properties;
+            i++
+                console.log("element no. "+i+" fetched")
+                return properties;
         });
     })
 
@@ -43,8 +47,11 @@ async function getTest(query){
 
 async function getAll(dateFrom = "dowolna", dateTo = "dowolna", fromWhere = [], toWhere = [], adults = 1, kids = [], order="najblizsze", pages=1){}
 
-tui.getOffers("2022-09-01","2023-06-01",[],["dom","egi-sha","egi-tab","dupa"],2,["2013-12-02","2011-03-11","2006-10-28"],"priceAsc",30)
-rainbow.getOffers("2022-11-01","2022-12-31",["WAW","WMI"],["his"],1,["2005-05-05"],"",60)
+// tui.getOffers("2022-09-01","2023-06-01",[],["dom","egi-sha","egi-tab","dupa"],2,["2013-12-02","2011-03-11","2006-10-28"],"priceAsc",30)
+// rainbow.getOffers("2022-11-01","2022-12-31",["WAW","WMI"],["his"],1,["2005-05-05"],"",60)
 // console.log(.then)
 
-// tui.getOffers("2022-11-01","2022-12-31",["WAW","WMI"],["dom"],2,["2005-05-05","2006-06-06"],"",60)
+// itaka.getOffers("2022-12-16","2023-01-6",[],["egi-mar"],2,["2005-05-05","2006-06-06"],"",51).then((e)=>{console.log(e)})
+let diagStart = new Date();
+itaka.getOffers("2022-11-01","2023-04-31",[],["his"],1,[],"",250).then(e=>{console.log(e);console.log(e.length);console.log("Query took "+((new Date()).getTime()-diagStart.getTime())+" seconds")})
+// console.log(getTest("lego"))
--- node-js/biura_podrozy/itaka.js
+++ node-js/biura_podrozy/itaka.js
@@ -1,15 +1,36 @@
 const puppeteer = require('puppeteer')
 
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight - window.innerHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+}
+
 async function getOffers(dateFrom, dateTo, fromWhere, toWhere, adults, kids, order, results){
     
+    let url = "https://www.itaka.pl/wyniki-wyszukiwania/wakacje/?view=offerList"
+
     if(dateFrom!=""){
         // let _dateFrom = new Date(dateFrom);
         let _dateFrom = new Date(dateFrom) < new Date() ? new Date() : new Date(dateFrom);
-        console.log(`&date-from=${_dateFrom.getFullYear()}-${("0"+(_dateFrom.getMonth()+1)).slice(-2)}-${("0"+_dateFrom.getDate()).slice(-2)}`)
+        url += `&date-from=${_dateFrom.getFullYear()}-${("0"+(_dateFrom.getMonth()+1)).slice(-2)}-${("0"+_dateFrom.getDate()).slice(-2)}`
     }
     if(dateTo!=""){
         let _dateTo = new Date(dateTo);
-        console.log(`&date-to=${_dateTo.getFullYear()}-${("0"+(_dateTo.getMonth()+1)).slice(-2)}-${("0"+_dateTo.getDate()).slice(-2)}`)
+        url += `&date-to=${_dateTo.getFullYear()}-${("0"+(_dateTo.getMonth()+1)).slice(-2)}-${("0"+_dateTo.getDate()).slice(-2)}`
     }
     
     let startPoints = []
@@ -37,7 +58,7 @@ async function getOffers(dateFrom, dateTo, fromWhere, toWhere, adults, kids, ord
         } 
     }
     if(startPoints!=[]){
-        console.log("&dep-region="+startPoints.join(","));
+        url += "&dep-region="+startPoints.join(",")
     }
     
     let destinations = []
@@ -119,8 +140,106 @@ async function getOffers(dateFrom, dateTo, fromWhere, toWhere, adults, kids, ord
         }    
     }
     if(destinations!=[]){
-        console.log("&dest-region="+destinations.join(","));
+        url += "&dest-region="+destinations.join(",")
     }
+
+    let _kids = "&kids="+kids.join(",");
+
+
+    let _adults = "&adults="+adults;
+
+    if(order=="priceAsc"){
+        url += "&order=priceAsc"
+    }else if(order=="priceDesc"){
+        url += "&order=priceDesc"
+    }else if(order=="dateAsc"||order==""){
+        url += "&order=dateFromAsc"
+    }
+
+    console.log(url)
+
+    const browser = await puppeteer.launch({
+        // headless: false,
+        defaultViewport: '',
+    });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    await page.waitForSelector(".details_save--1ja7w")
+    page.evaluate(_ => {
+        document.querySelector(".details_save--1ja7w").click()
+    })
+
+    if(!(await page.waitForSelector(".offer"))){
+        return {}
+    }
+
+    await autoScroll(page);
+
+    while(await page.evaluate(_=>{return document.querySelectorAll('.offers').length}) < results){
+        await page.evaluate(_=>{
+            for(let el of document.querySelectorAll('.offers')){
+                el.classList.add('prepared')
+            }
+        })
+        if(await page.evaluate(_=>{return document.querySelectorAll('.offer-list_more-offers').length==0})) break;false        
+        await page.evaluate(_=>{document.querySelector(".offer-list_more-offers").click()})
+        await page.waitForSelector(".offer:not(.prepared)")
+        await autoScroll(page)
+    }
+
+    page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
+    const offers = (await page.$$eval(".offer", elements => {
+            return elements.map(el => {
+                const properties = {};
+                
+                const title = el.querySelector(".header_title > a").innerHTML;
+                properties.title = title;
+                
+                // const stars = (el.querySelector(".header_stars_link > span").querySelectorAll(".star").length - (el.querySelector(".header_stars_link > span").querySelectorAll(".star_half").length/2)) || undefined;
+                // properties.stars = stars;
+
+                const location = el.querySelector(".header_geo-labels").innerText || el.querySelector(".header_geo-labels").textContent;;
+                properties.location = location;
+
+                const price = parseFloat(el.querySelector(".current-price_value").innerText.replace(" ",""));
+                properties.price = price;
+
+                const oldPrice = el.querySelector(".old-price_value")==null ? null : parseFloat(el.querySelector(".old-price_value").innerText.replace(" ",""))
+                properties.oldPrice = oldPrice;
+
+                const dates = el.querySelector(".offer_date > .offer_date_icon-container + span").innerText.split(" (")[0].split("-")
+                const dateFrom = dates[0].length==5?dates[0]+=dates[1].substring(5):dates[0]
+                const dateTo = dates[1]
+                const timeFrom = Date.parse(`20${dateFrom.substring(6)}-${dateFrom.substring(3,5)}-${dateFrom.substring(0,2)}`)
+                const timeTo = Date.parse(`20${dateTo.substring(6)}-${dateTo.substring(3,5)}-${dateTo.substring(0,2)}`)
+                properties.timeFrom = timeFrom
+                properties.timeTo = timeTo
+
+                const food = el.querySelector(".offer_food").innerText
+                properties.food = food
+
+                const offerLink = el.querySelector(".offer_link").href
+                properties.offerLink = offerLink
+
+
+                try{
+                    const imageLink = el.querySelector(".figure_main-photo").src || el.querySelector(".item_photo-img").src || null
+                    properties.imageLink = imageLink
+                }catch(e){
+                    console.log(e)
+                }
+
+
+                return properties;
+            });
+        }))
+
+        
+
+    browser.close()
+    // return offers.slice(0,results)
+    return offers
 }
 
 console.log("Zaimportowano skrypt dla biura podrozy Itaka")
